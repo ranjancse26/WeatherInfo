57904c156eefd2f5338e9de346a98764dc158343
ranjancse <ranjand@live.com>
2013-08-23 20:34:16 -0500

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherInfo.Models;
+
+namespace WeatherInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View(new WeatherInfoViewModel());
+        }
+
+        [HttpPost]
+        public ActionResult Index(WeatherInfoViewModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return View( new WeatherInfoViewModel
+                {
+                    WeatherCode = model.WeatherCode,
+                    WeatherInfo = null
+                });
+            }
+            var weatherBug = new WeatherBug.WeatherBug();
+            var info = weatherBug.checkWeather(WeatherBug.WeatherTypeEnum.ZipCode, model.WeatherCode.ToString());
+            var viewModel = new WeatherInfoViewModel
+            {
+                WeatherCode = model.WeatherCode,
+                WeatherInfo = info
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Models/WeatherInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WeatherInfo.Models
+{
+    public class WeatherInfoViewModel
+    {
+        public WeatherBug.WeatherInfo WeatherInfo { get; set; }
+        [Required]
+        public int WeatherCode { get; set; }
+    }
+}
